777b20926b826cb4ef18677de13351994cfd11c1
Alejo Agasi <102525836+Alejo55@users.noreply.github.com>
2026-01-11 14:24:33 -0300

    cors *

--- rxconfig.py
+++ rxconfig.py
@@ -3,11 +3,7 @@ import reflex as rx
 config = rx.Config(
     app_name="python_web",
     # Lista de dominios que pueden interactuar con tu backend
-    cors_allowed_origins=[
-        "http://localhost:3000",
-        "https://mi-pagina-web-personal.vercel.app",  # Reemplaza con tu URL real de Vercel
-        "https://python-web-cyan-sun.reflex.run",
-    ],
+    cors_allowed_origins=["*"],
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
