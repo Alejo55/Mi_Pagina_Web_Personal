133e183ebd4d2ec595458ae90fa17357eac577cb
Alejo Agasi <102525836+Alejo55@users.noreply.github.com>
2026-01-11 12:28:43 -0300

    agregado de reflex_api_url

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@ pip install --upgrade pip
 pip install -r requirements.txt
 rm -rf public
 reflex init
+export REFLEX_API_URL=https://289c1fc7-fb0e-4c12-8471-4106c46a6d05.fly.dev
 reflex export --frontend-only
 unzip frontend.zip -d public
 rm -f frontend.zip
