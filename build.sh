aa1717592828cd3e791e6cc1cbf73ee0ea410a0a
Alejo Agasi <102525836+Alejo55@users.noreply.github.com>
2026-01-11 14:40:24 -0300

    api_url en rxconfig

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ pip install --upgrade pip
 pip install -r requirements.txt
 rm -rf public
 reflex init
-REFLEX_API_URL=https://289c1fc7-fb0e-4c12-8471-4106c46a6d05.fly.dev reflex export --frontend-only
+reflex export --frontend-only
 unzip frontend.zip -d public
 rm -f frontend.zip
--- rxconfig.py
+++ rxconfig.py
@@ -2,6 +2,8 @@ import reflex as rx
 
 config = rx.Config(
     app_name="python_web",
+    # FORZAMOS LA API URL AQUÍ
+    api_url="https://289c1fc7-fb0e-4c12-8471-4106c46a6d05.fly.dev",
     # Lista de dominios que pueden interactuar con tu backend
     cors_allowed_origins=["*"],
     plugins=[
