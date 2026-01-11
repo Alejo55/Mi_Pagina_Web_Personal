6d791a06ba631e353c277b3ef409d751a1dde0f6
Alejo Agasi <102525836+Alejo55@users.noreply.github.com>
2026-01-11 12:56:56 -0300

    intento de resolucion de aviso rojo

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@ pip install --upgrade pip
 pip install -r requirements.txt
 rm -rf public
 reflex init
-export REFLEX_API_URL=https://289c1fc7-fb0e-4c12-8471-4106c46a6d05.fly.dev
-reflex export --frontend-only
+REFLEX_API_URL=https://289c1fc7-fb0e-4c12-8471-4106c46a6d05.fly.dev reflex export --frontend-only
 unzip frontend.zip -d public
 rm -f frontend.zip
--- rxconfig.py
+++ rxconfig.py
@@ -1,7 +1,15 @@
 import reflex as rx
 
-config = rx.Config(app_name="python_web",
-                   plugins=[
-                       rx.plugins.SitemapPlugin(),
-                       rx.plugins.TailwindV4Plugin(),
-                   ])
+config = rx.Config(
+    app_name="python_web",
+    # Lista de dominios que pueden interactuar con tu backend
+    cors_allowed_origins=[
+        "http://localhost:3000",
+        "https://mi-pagina-web-personal.vercel.app",  # Reemplaza con tu URL real de Vercel
+        "https://python-web-cyan-sun.reflex.run",
+    ],
+    plugins=[
+        rx.plugins.SitemapPlugin(),
+        rx.plugins.TailwindV4Plugin(),
+    ],
+)
