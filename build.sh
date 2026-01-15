186500fe316e5f3a1d462de5bef2e08e72e4d678
Alejo Agasi <102525836+Alejo55@users.noreply.github.com>
2026-01-15 12:52:42 -0300

    Actualizacion build con api_url

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ pip install --upgrade pip
 pip install -r requirements.txt
 rm -rf public
 reflex init
-reflex export --frontend-only
+API_URL=https://alejoagasiweb.up.railway.app reflex export --frontend-only
 unzip frontend.zip -d public
 rm -f frontend.zip
