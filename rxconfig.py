b748ff6bc5b24939a77948260c6a0309dfced01e
Alejo Agasi <102525836+Alejo55@users.noreply.github.com>
2026-01-15 13:12:55 -0300

    Actualizacion rxconfig

--- rxconfig.py
+++ rxconfig.py
@@ -3,9 +3,13 @@ import reflex as rx
 config = rx.Config(
     app_name="python_web",
     # Lista de dominios que pueden interactuar con tu backend
-    # cors_allowed_origins=["*"],
+    cors_allowed_origins=[
+        "http://localhost:3000",
+        "https://mi-pagina-web-personal.vercel.app",
+    ],
     plugins=[
         rx.plugins.SitemapPlugin(),
-        rx.plugins.TailwindV4Plugin(),
+        rx.plugins.TailwindV4Plugin(),  # Mejor para el diseño actual
     ],
+    show_built_with_reflex=False,  # Para que se vea más profesional (sin el logo de Reflex)
 )
